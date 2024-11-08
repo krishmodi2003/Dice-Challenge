142999d03deb027598bd28e2c8fb17c0b87b4ab3
krishmodi2003 <61406938+krishmodi2003@users.noreply.github.com>
2024-11-08 13:24:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var dice2= document.querySelector('.img2').setAttribute('src','./images/'+'dice'
 
 if (randomNumber1>randomNumber2){
     document.querySelector('h1').innerHTML='Player 1 Won';
-} if(randomNumber1<randomNumber2){
+} else if(randomNumber1<randomNumber2){
             document.querySelector('h1').innerHTML='Player 2 Won'
 } else{
     document.querySelector('h1').innerHTML='Draw!';
